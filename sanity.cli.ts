4b6ba31a9359cb3d9d2faf99d3a00351212c76df
Steven H-A <stevenkejjad@gmail.com>
2026-05-26 22:20:52 +0100

    Add Sanity deployment appId to cli config

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -5,4 +5,7 @@ export default defineCliConfig({
     projectId: process.env.SANITY_STUDIO_PROJECT_ID ?? "vo8cimnh",
     dataset: process.env.SANITY_STUDIO_DATASET ?? "production",
   },
+  deployment: {
+    appId: "e30pbcgtyw67ud5vgr7c05kc",
+  },
 });
